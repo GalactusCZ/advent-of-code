216c373cd3e54d58a73456af281bda041496f56e
Michael Hrubý <galactus2121@gmail.com>
2024-12-09 14:11:48 +0100

    Day 9 WIP 3

--- 2024/day_9/day_9_2.py
+++ 2024/day_9/day_9_2.py
@@ -1,78 +1,33 @@
-Divided = list[tuple[int, bool, int]]
+Divided = list[tuple[int, int, int]]
 
-def reorder(divided: Divided) -> Divided:
-    end: Divided = []
-    start: Divided = []
-    curr_divided = divided.copy()
+def calculate(files: list[Divided], spaces: list[Divided]) -> int:
+    result = 0
 
-    while len(curr_divided) > 1:
-        print(len(curr_divided))
-        amount, is_num, mult = curr_divided.pop()
-
-        if not is_num:
-            end.append((amount, is_num, mult))
-
-        else:
-            changes = False
-
-            while not changes and len(curr_divided) > 0:
-                in_amount, in_is_num, _ = curr_divided[0]
-
-                if in_is_num:
-                    start.append(curr_divided[0])
-                else:
-                    if in_amount >= amount:
-                        changes = True
-                        start.append((amount, True, mult))
-
-                        if in_amount > amount:
-                            start.append((in_amount - amount, False, -1))
-
-                    else:
-                        start.append(curr_divided[0])
-
-                curr_divided = curr_divided[1:]
-
-            if changes:
-                end.append((amount, False, -1))
-
-            else:
-                end.append((amount, True, mult))
-
-            start.extend(curr_divided)
-            curr_divided = start
-            start = []
-
-    if len(curr_divided) > 0:
-        end.extend(curr_divided)
-
-    end.reverse()
-    return end
+    for i, (f_amount, f_index, f_ci) in enumerate(files):
+        for j, (s_amount, s_index, s_ci) in enumerate(spaces):
+            if f_ci < s_ci:
+                spaces = spaces[:j]
+                break
+                
+            if s_amount >= f_amount:
 
 
-with open("day_9.txt") as f:
+with open("day_9_test.txt") as f:
     line = f.readline()
-    divided: Divided = []
-    multiplier = 0
+    files: list[Divided] = []
+    spaces: list[Divided] = []
+    c_index = 0
+    index = 0
 
     for i, char in enumerate(line):
         if i % 2 == 0:
-            divided.append((int(char), True, multiplier))
-            multiplier += 1
+            files.append((int(char), index, c_index))
+            index += 1
 
         else:
-            divided.append((int(char), False, -1))
+            spaces.append((int(char), -1, c_index))
+            
+        c_index += int(char)
 
-
-    reor = reorder(divided)
-    multiplier = 0
-    result = 0
-
-    for amount, is_num, index in reor:
-        for _ in range(amount):
-            if is_num:
-                result += index * multiplier
-
-            multiplier += 1
-
-    print(result)
+    files.reverse()
+    print(calculate(files, spaces))
