6c47c1ee628d02554b3af37536ca8edf7ba7a78a
Michael Hruby <galactus2121@gmail.com>
2024-12-09 14:24:26 +0100

    Day 9 Finished

--- 2024/day_9/day_9_2.py
+++ 2024/day_9/day_9_2.py
@@ -4,15 +4,30 @@ def calculate(files: list[Divided], spaces: list[Divided]) -> int:
     result = 0
 
     for i, (f_amount, f_index, f_ci) in enumerate(files):
+        print(f"{i}/{len(files)}")
+        moved = False
+
         for j, (s_amount, s_index, s_ci) in enumerate(spaces):
             if f_ci < s_ci:
                 spaces = spaces[:j]
                 break
                 
             if s_amount >= f_amount:
+                for k in range(f_amount):
+                    result += (s_ci + k) * f_index
+
+                spaces[j] = (s_amount - f_amount, s_index, s_ci + f_amount)
+                moved = True
+                break
+
+        if not moved:
+            for k in range(f_amount):
+                result += (f_ci + k) * f_index
+
+    return result
 
 
-with open("day_9_test.txt") as f:
+with open("day_9.txt") as f:
     line = f.readline()
     files: list[Divided] = []
     spaces: list[Divided] = []
