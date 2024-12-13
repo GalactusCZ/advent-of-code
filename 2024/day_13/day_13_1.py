757d068f0dc5537584f7fc44059143569df3c5cf
Michael Hruby <galactus2121@gmail.com>
2024-12-13 22:10:17 +0100

    Day 13 WIP 2

--- 2024/day_13/day_13_1.py
+++ 2024/day_13/day_13_1.py
@@ -1,6 +1,26 @@
-def check_machines()
+def check_machines(machine: dict[str, int]) -> int:
+    p_x, p_y = machine['P']
+    a_x, a_y = machine['A']
+    b_x, b_y = machine['B']
+    b = 0
+    
+    while b * b_x <= p_x and b * b_y <= p_y:
+        if p_x == b_x * b and p_y == b_y * b:
+            return b
 
-with open("day_13_test.txt") as f:
+        a = 0
+        while a * a_x <= p_x and a * a_y <= p_y:
+            if p_x == a_x * a + b_x * b and p_y == a_y * a + b_y * b:
+                return b + a * 3
+
+            a += 1
+
+        b += 1
+
+    return 0
+
+
+with open("day_13.txt") as f:
     machines: list[dict[str, int]] = []
     curr_m: dict[str, int] = {}
     
@@ -23,14 +43,12 @@ with open("day_13_test.txt") as f:
             line = spliter[1].split(', ')
             
             curr_m[spliter[0]] = (int(line[0].split('+')[1]), int(line[1].split('+')[1]))
-            
+
+    result = 0
+
     for machine in machines:
-            p_x, p_y = machine['P']
-            a_x, a_y = machine['A']
-            b_x, b_y = machine['B']
-            
-            for a in range(101):
-                
-                
-                for b in range(1, 101):
-            
+        test = check_machines(machine)
+        print(test, machine)
+        result += test
+
+    print(result)
